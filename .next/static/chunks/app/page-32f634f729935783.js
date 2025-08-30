3cfa379192ad31850f62931c2fbd39c2a9e0d6d2
Avokadiy <braaedy1@mail.ru>
2025-08-30 15:02:51 +0500

    build

---
+++ .next/static/chunks/app/page-32f634f729935783.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[974],{694:(e,s,n)=>{Promise.resolve().then(n.bind(n,1066)),Promise.resolve().then(n.bind(n,8309))}},e=>{var s=s=>e(e.s=s);e.O(0,[213,670,444,773,937,416,441,684,358],()=>s(694)),_N_E=e.O()}]);
